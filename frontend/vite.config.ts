3137301b4bf0a90d39d6b2e6fac039cb23004247
Jayden <jatychan@ucsc.edu>
2025-08-04 21:01:03 -0700

    Add Amplify deployment configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser',
+      },
+    ],
+  },
+  // Critical for Amplify CI/CD
+  build: {
+    outDir: 'dist'
+  }
 })
